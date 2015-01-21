e53b7889897fd3710d426a3d8c7c6f4cdf724c5c
Jay Laiche <laichejl@vt.edu>
2015-01-20 23:28:36 -0500

    grunt config and hello world

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt){
+
+	grunt.initConfig({
+
+		// concat: {
+		// 	js: {
+		// 		src: ['js/1.js', 'js/2.js'],
+		// 		dest: 'build/js/scripts.js',
+		// 	}
+		// },
+
+		jshint: {
+			all: ['Gruntfile.js', 'js/*.js']
+		},
+
+		watch: {
+			js: {
+				files: ['js/**/*.js'],
+				tasks: ['jshint'],
+			},
+		},
+
+		// uglify: {
+		// 	js: {
+		// 		files: {
+		// 			'build/js/scripts.min.js': ['build/js/scripts.js']
+		// 		}
+		// 	}
+		// }
+	});
+
+	//grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	//grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.registerTask('default', ['watch']);
+
+};
